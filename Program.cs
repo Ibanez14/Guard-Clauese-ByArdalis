3054730c75d2e17c75809a592565d7d8cb120f27
Shamilms <shamilms@code.edu.az>
2019-12-09 11:00:55 +0400

    Initia

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Ardalis.GuardClauses;
+using System;
+
+namespace Guard_Clause
+{
+    public class Foo
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Custom Guard Clauses
+            string value = Console.ReadLine();
+            CustomGuard.Throw.IfNull(value, value);
+
+            var foo = new Foo();
+            Guard.Against.Null(foo, foo.Name);
+            Guard.Against.NullOrEmpty(foo.Name, foo.Name);
+            //Guard.Against.OutOfRange();
+            //Guard.Against.NullOrWhiteSpace()
+            //Guard.Against.Default
+            Console.ReadLine();
+        }
+        
+    }
+
+    public static class CustomGuard
+    {
+        public class Throw
+        {
+            public static void IfNull(object argument, string argumentName)
+            {
+                if(argument == null)
+                    throw new ArgumentNullException(argumentName);   
+            }
+        }
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+Ardalis.GuardClauese
+
+Supported Guard Clauses
+
+    Guard.Against.Null (throws if input is null)
+    Guard.Against.NullOrEmpty (throws if string or array input is null or empty)
+    Guard.Against.NullOrWhiteSpace (throws if string input is null, empty or whitespace)
+    Guard.Against.OutOfRange (throws if integer/DateTime/enum input is outside a provided range)
+    Guard.Against.OutOfSQLDateRange (throws if DateTime input is outside the valid range of SQL Server DateTime values)
+    Guard.Against.Zero (throws if number input is zero)
+
+- Can extent with extension method
